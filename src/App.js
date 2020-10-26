a54463509e0b1c67d016d8a67993c340997f397d
unknown <frankmnganga@gmail.com>
2020-10-26 09:47:02 +0300

    Created the Home Page and Set up react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Game from './pages/Game';
 import { GameProvider } from './context/GameContext';
+import Home from './pages/Home';
+import './App.css';
 
 function App() {
 	return (
-		<GameProvider>
-			<Game />
-		</GameProvider>
+		<Router>
+			<GameProvider>
+				<Route path="/" exact component={Home} />
+				<Route path="/game" component={Game} />
+			</GameProvider>
+		</Router>
 	);
 }
 
